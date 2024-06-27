58d5426cdf869b96f3d28fa27b91a76570b38a98
s1f10210368 <s1f102103684@iniad.org>
2024-06-27 14:55:51 +0900

    R

--- test/Token.js
+++ test/Token.js
@@ -2,8 +2,14 @@ const { expect } = require("chai");
 
 describe("Token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
+    // ethers.jsのSignerはEthereumアカウントを表すオブジェクト、コントラクトや他のアカウントにトランザクションを送信するために使用される
+    // ここでは、接続しているノード(ここではHardhat Network)のアカウントリストを取得して最初のアカウントだけを保持している
     const [owner] = await ethers.getSigners();
 
+    // コードを常に明示的にしたい場合は以下の行を追加
+    /*const { ethers } = require("hardhat");*/
+
+    // 以下の行を呼び出すことで
     const hardhatToken = await ethers.deployContract("Token");
 
     const ownerBalance = await hardhatToken.balanceOf(owner.address);
